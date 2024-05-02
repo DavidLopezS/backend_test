22fad614cb385da5be76a151defe7911e6a95d4f
David Lopez Saludes <david.lopez@bim6d.es>
2024-05-02 11:22:41 +0200

    Got conexion with LinkedIn API

--- linkedin_api_logic.py
+++ linkedin_api_logic.py
@@ -1,13 +1,16 @@
 import requests
 
-access_token: str = "API_TOKEN"
-api_endpoint: str = "temp"
+rapid_api_key: str = "9f5cd37c5bmsh47e1ba15a30b7ecp1f27cfjsn5e640e1ac804"
+rapid_api_host: str = "linkedin-data-scraper.p.rapidapi.com"
+api_endpoint: str = "https://linkedin-data-scraper.p.rapidapi.com/person"
 
 def detect_job_change(linkedin_url: str):
-    headers = {"Authorization": f"Bearer {access_token}"}
-    params = {
-        'linkedin_porfile_url': f"{linkedin_url}",
-        'use-cache': 'if-recent'
-    }
-    response = requests.get(api_endpoint, params=params, headers=headers)
-    return f"New job detected: {response.json()['experiences'][0]['title']}" 
+    
+    headers = {
+        "content-type": "application/json",
+	    "X-RapidAPI-Key": f"{rapid_api_key}",
+	    "X-RapidAPI-Host": f"{rapid_api_host}"}
+    
+    payload = {"link": f"{linkedin_url}"}
+    response = requests.post(api_endpoint, json=payload, headers=headers)
+    return f"New job detected: {response.json()['data']['experiences'][0]['title']}" 
