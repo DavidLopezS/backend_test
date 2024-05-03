ebf9f592d713780a32704670f073968010dfaafa
David Lopez Saludes <david.lopez@bim6d.es>
2024-05-03 13:14:57 +0200

    Code cleaned-up and .env changed

--- app/classes.py
+++ app/classes.py
@@ -1,7 +1,10 @@
+import sqlite3
+import uuid
 import requests
 import os
 from pydantic import BaseModel
-from typing import Dict
+from typing import Dict, List, Optional
+from fastapi import HTTPException
 
 
 class UserProfile(BaseModel):
@@ -13,34 +16,37 @@ class EmailBody(BaseModel):
 
 class JobStructure(BaseModel):
     title: str
-    subtitle: str
-    caption: str
-    metadata: str
+    subtitle: Optional[str]
+    caption: Optional[str]
+    metadata: Optional[str]
 
     @classmethod
     def fetch_linkedin_profile(cls, linkedin_url: str):
-        rapid_api_key: str = os.environ.get('X_RAPID_API_KEY', '')
-        rapid_api_host: str = os.environ.get('X_RAPID_API_HOST', '')
-        api_endpoint: str = os.environ.get('URL', '')
-
-        headers: Dict[str, str] = {
-            "content-type": "application/json",
-            "X-RapidAPI-Key": f"{rapid_api_key}",
-            "X-RapidAPI-Host": f"{rapid_api_host}"
-        }
-        payload: Dict[str, str] = {"link": f"{linkedin_url}"}
-        
-        response = requests.post(api_endpoint, json=payload, headers=headers)
-
         try:
-            experiences = response.json()['data']['experiences']
+            rapid_api_key: str = os.environ['X_RAPID_API_KEY']
+            rapid_api_host: str = os.environ['X_RAPID_API_HOST']
+            api_endpoint: str = os.environ['URL']
+
+            headers: Dict[str, str] = {
+                "content-type": "application/json",
+                "X-RapidAPI-Key": rapid_api_key,
+                "X-RapidAPI-Host": rapid_api_host
+            }
+            payload: Dict[str, str] = {"link": linkedin_url}
+            
+            response = requests.post(api_endpoint, json=payload, headers=headers)
+            response.raise_for_status()
+        
+            experiences = response.json().get('data', {}).get('experiences', [])
             if experiences:
-                job_data = experiences[0]
+                job_data = experiences[job_num]
                 return cls(title=job_data.get('title'), subtitle=job_data.get('subtitle'), caption=job_data.get('caption'), metadata=job_data.get('metadata'))
             else:
-                return cls(title='No job found', subtitle='', caption='', metadata='')
+                return cls(title='No job found')
+        except requests.exceptions.RequestException as e:
+            return cls(title=f"Request error: {e}")
         except Exception as e:
-            return cls(title=f"Error parsing JSON: {e}", subtitle='', caption='', metadata='') 
+            return cls(title=f"Error: {e}") 
     
     def equals(self, other: 'JobStructure') -> bool:
         return(
@@ -49,3 +55,108 @@ class JobStructure(BaseModel):
             self.caption == other.caption and
             self.metadata == other.metadata
         )
+
+class Database:
+    def __init__(self, db_path: str):
+        self.db_path = db_path
+    
+    def get_connection(self):
+        return sqlite3.connect(self.db_path)
+    
+    def create_user_profiles_table(self):
+        conn = self.get_connection()
+        cursor = conn.cursor()
+        cursor.execute('''CREATE TABLE IF NOT EXISTS user_profiles (
+                              id TEXT PRIMARY KEY,
+                              email TEXT,
+                              linkedin_url TEXT,
+                              current_job_title TEXT,
+                              current_job_subtitle TEXT,
+                              current_job_caption TEXT,
+                              current_job_metadata TEXT
+                          )''')
+        conn.commit()
+        conn.close()
+    
+    def register_user(self, profile: UserProfile):
+        try:
+            conn = self.get_connection()
+            cursor = conn.cursor()
+
+            user_id = str(uuid.uuid4())
+            current_job = JobStructure.fetch_linkedin_profile(profile.linkedin_url)
+
+            cursor.execute('''INSERT INTO user_profiles (id, email, linkedin_url, current_job_title, current_job_subtitle, current_job_caption, current_job_metadata)
+                              VALUES (?, ?, ?, ?, ?, ?, ?)''',
+                              (user_id, profile.email, profile.linkedin_url, current_job.title, current_job.subtitle, current_job.caption, current_job.metadata))
+            
+            conn.commit()
+            conn.close()
+
+            return {"message": "User registered profile successfully"}
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=f"An error occured: {e}")
+    
+    def detect_job_changes(self, email_body: EmailBody):
+        try: 
+            conn = self.get_connection()
+            cursor = conn.cursor()
+
+            email = email_body.email
+
+            cursor.execute('SELECT * FROM user_profiles WHERE email=?', (email,))
+            user_profile = cursor.fetchone()
+            if not user_profile:
+                raise HTTPException(status_code=404, detail="User not found")
+            
+            linkedin_url = user_profile[2]
+            user_new_job = JobStructure.fetch_linkedin_profile(linkedin_url)
+
+            if user_profile[3:7] == (user_new_job.title, user_new_job.subtitle, user_new_job.caption, user_new_job.metadata):
+                conn.close()
+
+                return {
+                    "message": "No job change detected",
+                    "User_job": {
+                        "title": user_profile[3],
+                        "subtitle": user_profile[4],
+                        "caption": user_profile[5],
+                        "metadata": user_profile[6]
+                    }
+                }
+            else:
+                cursor.execute('''UPDATE user_profiles
+                                SET current_job_title=?, current_job_subtitle=?, current_job_caption=?, current_job_metadata=?
+                                WHERE email=?''',
+                                (user_new_job.title, user_new_job.subtitle, user_new_job.caption, user_new_job.metadata, email))
+                conn.commit()
+                conn.close()
+
+                return {
+                    "message": "Job change detected",
+                    "User_job": {
+                        "title": user_new_job.title,
+                        "subtitle": user_new_job.subtitle,
+                        "caption": user_new_job.caption,
+                        "metadata": user_new_job.metadata
+                    }
+                }
+        except Exception as e: 
+            raise HTTPException(status_code=500, detail=f"An error occured: {e}")
+    
+    def get_user_profiles(self) ->List[Dict]:
+        try:
+            conn = self.get_connection()
+            cursor = conn.cursor()
+
+            cursor.execute('SELECT * FROM user_profiles')
+            user_profiles = cursor.fetchall()
+            conn.close()
+
+            if user_profiles:
+                return user_profiles
+            else:
+                raise HTTPException(status_code=404, detail="No users registered")
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=f"An error occurred: {e}")
+
--- app/main.py
+++ app/main.py
@@ -1,8 +1,5 @@
-import sqlite3
-import uuid
-from fastapi import FastAPI, HTTPException
-from typing import Dict
-from app.classes import UserProfile, EmailBody, JobStructure
+from fastapi import FastAPI
+from app.classes import UserProfile, EmailBody, Database
 from fastapi.middleware.cors import CORSMiddleware
 
 app: FastAPI = FastAPI()
@@ -16,109 +13,19 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-def get_db_connection():
-    return sqlite3.connect('user_profiles.db')
+DB_PATH = "user_profiles.db"
 
-def create_user_profiles_table():
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    cursor.execute('''CREATE TABLE IF NOT EXISTS user_profiles (
-                          id TEXT PRIMARY KEY,
-                          email TEXT,
-                          linkedin_url TEXT,
-                          current_job_title TEXT,
-                          current_job_subtitle TEXT,
-                          current_job_caption TEXT,
-                          current_job_metadata TEXT
-                      )''')
-    conn.commit()
-    conn.close()
-
-create_user_profiles_table()
+db = Database(DB_PATH)
+db.create_user_profiles_table()
 
 @app.post("/register")
 def register_user(profile: UserProfile):
-    try:
-        conn = get_db_connection()
-        cursor = conn.cursor()
-
-        id: str = str(uuid.uuid4())
-        current_job: JobStructure = JobStructure.fetch_linkedin_profile(profile.linkedin_url)
-
-        cursor.execute('''INSERT INTO user_profiles (id, email, linkedin_url, current_job_title, current_job_subtitle, current_job_caption, current_job_metadata)
-                          VALUES (?, ?, ?, ?, ?, ?, ?)''',
-                          (id, profile.email, profile.linkedin_url, current_job.title, current_job.subtitle, current_job.caption, current_job.metadata))
-        
-        conn.commit()
-        conn.close()
-
-        return {"message": "User registered profile successfully"}
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"An error occured: {e}") 
+    return db.register_user(profile)
 
 @app.post("/detect_job_changes")
 def trigger_job_change_detection(email_body: EmailBody):
-    try: 
-        conn = get_db_connection()
-        cursor = conn.cursor()
-
-        email: str = email_body.email
-
-        cursor.execute('SELECT * FROM user_profiles WHERE email=?', (email,))
-        user_profile = cursor.fetchone()
-        if not  user_profile:
-            raise HTTPException(status_code=404, detail="User not found")
-        
-        linkedin_url: str = user_profile[2]
-        user_new_job: JobStructure = JobStructure.fetch_linkedin_profile(linkedin_url)
-
-        if user_profile[3:7] == (user_new_job.title, user_new_job.subtitle, user_new_job.caption, user_new_job.metadata):
-            conn.close()
-
-            return {
-                "message": "No job change detected",
-                "User_job": {
-                    "title": user_profile[3],
-                    "subtitle": user_profile[4],
-                    "caption": user_profile[5],
-                    "metadata": user_profile[6]
-                }
-            }
-        else:
-            cursor.execute('''UPDATE user_profiles
-                            SET current_job_title=?, current_job_subtitle=?, current_job_caption=?, current_job_metadata=?
-                            WHERE email=?''',
-                            (user_new_job.title, user_new_job.subtitle, user_new_job.caption, user_new_job.metadata, email))
-            conn.commit()
-            conn.close()
-
-            return {
-                "message": "Job change detected",
-                "User_job": {
-                    "title": user_new_job.title,
-                    "subtitle": user_new_job.subtitle,
-                    "caption": user_new_job.caption,
-                    "metadata": user_new_job.metadata
-                }
-            }
-    except Exception as e: 
-        raise HTTPException(status_code=500, detail=f"An error occured: {e}") 
-
+    return db.detect_job_changes(email_body)
 
 @app.get("/user_profiles")
 def get_user_profiles():
-    try:
-        conn = get_db_connection()
-        cursor = conn.cursor()
-
-        cursor.execute('SELECT * FROM user_profiles')
-        user_profiles = cursor.fetchall()
-        conn.close()
-
-        if user_profiles:
-            return user_profiles
-        else:
-            raise HTTPException(status_code=404, detail="No users registered")
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"An error occurred: {e}")
-
+   return db.get_user_profiles()
